20bdc3a8053afb6b1630dc6a7b07903f395eb71b
shimuli <shimulicedric@gmail.com>
2019-08-27 15:20:08 +0300

    another one

---
+++ resources/views/submit.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+@section('content')
+<div class ="container">
+<div class ="row">
+<h1>submit a link</h1>
+<form action="/submit" method ="post">
+@if($error->any())
+<div class="Alert alert-danger" role ="alert">
+Please fix the following error_get_last
+</div>
+@endlif
+
+
+{!! csrf_field() !!}
+                <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Title" value="{{ old('title') }}">
+                    @if($errors->has('title'))
+                        <span class="help-block">{{ $errors->first('title') }}</span>
+                    @endif
+                </div>
+                <div class="form-group{{ $errors->has('url') ? ' has-error' : '' }}">
+                    <label for="url">Url</label>
+                    <input type="text" class="form-control" id="url" name="url" placeholder="URL" value="{{ old('url') }}">
+                    @if($errors->has('url'))
+                        <span class="help-block">{{ $errors->first('url') }}</span>
+                    @endif
+                </div>
+                <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
+                    <label for="description">Description</label>
+                    <textarea class="form-control" id="description" name="description" placeholder="description">{{ old('description') }}</textarea>
+                    @if($errors->has('description'))
+                        <span class="help-block">{{ $errors->first('description') }}</span>
+                    @endif
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+    </div>
+@endsection
+
+
